bd60a6cca38560a21308a837d7124d7d27020e5a
Kumar Kartikey Choudhary <choudharykartik733@gmail.com>
2025-04-20 11:06:55 +0530

    Add files via upload

---
+++ LargestString.java
@@ -0,0 +1,19 @@
+package String;
+
+public class LargestString {
+
+	public static void main(String args[])
+	{
+		String [] fruits = { "Apple","mango","Banana"};
+		
+		String largest = fruits[0];
+		
+		for(int i = 1 ; i < fruits.length; i++)
+		{
+			if(largest.compareToIgnoreCase(fruits[i]) < 0 )
+				largest = fruits[i];
+		}
+		
+		System.out.println(largest);
+	}
+}
